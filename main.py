ab4cda32b58921f046b3bf25f415e9cf0147da9c
Jack <jack.aldus@gmail.com>
2026-04-20 20:41:12 +0100

    git commit "A: Added user inputs with argparse library, converted basic string input to take a 'messages' conversation useing google.genai library improt as 'contents=' input in generate_content() call'

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -11,12 +13,32 @@ def main():
     if api_key is None:
         raise RuntimeError("api_key missing. check .env in project root directory")
 
+    # argparse added, creates the ability for a users input to be pass to "content=" inside
+    # .generate_content to be passed into the api for the AI model.
+    parser = argparse.ArgumentParser(
+        description="Chatbot"
+    )  # create parser object (imported from argparse)
+    parser.add_argument(
+        "user_prompt", type=str, help="User Prompt"
+    )  # expect one argument called "user_prompt" which is a string
+    args = parser.parse_args()  # read user input string from CLI
+
+    # Creating list/ conversation history
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # Main AI access code block
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        model="gemini-2.5-flash", contents=messages
     )
-    print(response.text)
+
+    if response.usage_metadata is None:
+        raise RuntimeError("Gemini Api response metadata missing or malformed")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(f"{response.text}")
 
 
 if __name__ == "__main__":
